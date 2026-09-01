dd5d69ddcbdee9f5ecb86395f5d4085beaaf5965
Shahriar Hossain Fahim <shfahim.dev@gmail.com>
2026-09-01 22:27:39 +0600

    Add: longest-consecutive-sequence - submission-0

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-0.js
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums) {
+      let hash = new Set(nums);
+      let starts = [];
+      nums.forEach(n => {
+        if(!hash.has(n - 1)) {
+          starts.push(n);
+        }
+      })
+      console.log(starts);
+
+      let counter = 0;
+      let i = 0;
+
+      while(i < starts.length) {
+        let turn = 1;
+        while(turn > 0) {
+          if (hash.has(starts[i] + turn) === false) {
+            if(counter < turn) {
+              counter = turn;
+            }
+            turn = 0;
+            continue;
+          }
+          turn++;
+        }
+        i++;
+      }
+
+      return counter;
+    }
+}
