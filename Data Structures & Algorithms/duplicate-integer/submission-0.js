ea2aaee4fe853a9125a166d56fd1b1acf5733a0b
Shahriar Hossain Fahim <shfahim.dev@gmail.com>
2026-09-05 00:32:34 +0600

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.js
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+      let hash = new Set();
+      for(let i=0; i < nums.length; i++) {
+        if(hash.has(nums[i])) return true;
+        hash.add(nums[i]);
+      }
+      return false;
+    }
+}
