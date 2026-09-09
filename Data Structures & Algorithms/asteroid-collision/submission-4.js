b60c2467a0d2799ea3237afd06ab90d8cf55610d
Shahriar Hossain Fahim <shfahim.dev@gmail.com>
2026-09-10 02:13:16 +0600

    Add: asteroid-collision - submission-4

---
+++ Data Structures & Algorithms/asteroid-collision/submission-4.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} asteroids
+     * @return {number[]}
+     */
+    asteroidCollision(asteroids) {
+      let stack = [];
+      asteroids.forEach(a => {
+        while(stack[0] && a < 0 && stack[stack.length - 1] > 0) {
+          let diff = a + stack[stack.length - 1];
+          if(diff < 0) {
+            stack.pop();
+          } else if (diff > 0) {
+            a = 0;
+          } else {
+            stack.pop();
+            a = 0;
+          }
+        }
+        if(a !== 0) {
+          stack.push(a);
+        }
+      })
+      return stack;
+    }
+}
